2d9d6391cd9c5a5df02a6367060081b7876d1006
matanui159 <josh@minteronline.com>
2018-09-10 17:35:40 +1000

    Removed mesh and fixed ogl gen

--- src/build/mesh_data.h
+++
@@ -1,6 +0,0 @@
-#ifndef MESH_DATA_H_
-#define MESH_DATA_H_
-
-void mesh_data();
-
-#endif
--- src/build/ogl.c
+++ src/build/ogl.c
@@ -2,14 +2,15 @@
 #include "util.h"
 #include <ctype.h>
 
-static void gen_type(const char* func, array_t* type) {
+static void gen_type(array_t* func, array_t* type) {
 	type->size = 0;
 	util_string(type, 'P');
 	util_string(type, 'F');
 	util_string(type, 'N');
 
-	for (; func != '\0'; ++func) {
-		util_string(type, toupper(*func));
+	for (int i = 0; i < func->size; ++i) {
+		char* c = array_get(func, i);
+		util_string(type, toupper(*c));
 	}
 
 	util_string(type, 'P');
@@ -34,28 +35,28 @@ void ogl() {
 	util_output(out, "#include <GL/glext.h>");
 
 	while (util_input(in, &func)) {
-		gen_type(func.data, &type);
-		output_write(out, "extern %s ogl_%s;", type.data, func.data);
-		output_write(out, "#define %s ogl_%s", func.data, func.data);
+		gen_type(&func, &type);
+		util_output(out, "extern %s ogl_%s;", type.data, func.data);
+		util_output(out, "#define %s ogl_%s", func.data, func.data);
 	}
 
-	output_write(out, "void ogl_init();");
-	output_write(out, "#endif");
-	output_write(out, "#ifdef OGL_BUILD");
+	util_output(out, "void ogl_init();");
+	util_output(out, "#endif");
+	util_output(out, "#ifdef OGL_BUILD");
 	
 	while (util_input(in, &func)) {
-		gen_type(func.data, &type);
-		output_write(out, "%s ogl_%s;", type.data, func.data);
+		gen_type(&func, &type);
+		util_output(out, "%s ogl_%s;", type.data, func.data);
 	}
 
-	output_write(out, "void ogl_init() {");
+	util_output(out, "void ogl_init() {");
 
-	while (input_read(in, &func)) {
-		gen_type(func.data, &type);
-		output_write(out, "\togl_%s = (%s)context_func(\"%s\");",
+	while (util_input(in, &func)) {
+		gen_type(&func, &type);
+		util_output(out, "\togl_%s = (%s)context_func(\"%s\");",
 			func.data, type.data, func.data);
 	}
 
-	output_write(out, "}");
-	output_write(out, "#endif");
+	util_output(out, "}");
+	util_output(out, "#endif");
 }
